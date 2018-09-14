54836e6d3f3328dc0a9ead1f94b08e21094c9fbc
shrekuu <shrekuu@163.com>
2018-09-14 17:48:12 +0800

    added beginner tutorial exercise for docker and docker-composer

---
+++ docker-compose-in-12-minutes/website/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>My Shop</title>
+</head>
+<body>
+    
+    <h1>Welcome to my shop</h1>
+    <ul>
+        <?php
+            $json = file_get_contents('http://product-service');
+            $obj = json_decode($json);
+
+            $products = $obj->products;
+            foreach ($products as $product) {
+                echo "<li>$product</li>";
+            }
+        ?>
+    </ul>
+
+</body>
+</html>
---
+++ readme.md
@@ -0,0 +1,7 @@
+### Jake Wright Docker Tutorial Exercises
+
+Learn Docker in 12 Minutes 🐳
+https://www.youtube.com/watch?v=YFl2mCHdv24
+
+Docker Compose in 12 Minutes
+https://www.youtube.com/watch?v=Qw9zlE3t8Ko
